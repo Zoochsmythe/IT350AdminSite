e460f2f16851f6ec174bf3313739d8f6537ba41b
Zoochsmythe <34142356+Zoochsmythe@users.noreply.github.com>
2018-03-10 22:55:29 -0800

    Add files via upload

--- admin.php
+++ admin.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
 
     <title>Hello, world!</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Hi-Tech Communications</a>
+      <a class="navbar-brand" href="Index.php">Hi-Tech Communications</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,23 +20,28 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="Index.php">Home <span class="sr-only">(current)</span></a>
           </li>
 
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="Index.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Buy/Sell
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Buy Phone</a>
-              <a class="dropdown-item" href="#">Sell Phone</a>
+              <a class="dropdown-item" href="Index.php">Buy Phone</a>
+              <a class="dropdown-item" href="Index.php">Sell Phone</a>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="Index.php">Something else here</a>
+
             </div>
           </li>
         </ul>
       <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Admin Login</a></li>    
+      <li><a class="nav-link" href="displaytables.php">Display Tables</a></li>
+      <li><a class="nav-link" href="AddEditPhones.php">Add/Edit Phone</a></li>
+      <li><a class="nav-link" href="#">Add/Edit Customer</a></li>
+      <li><a class="nav-link" href="#">Add/Edit Order</a></li>
+      <li><a class="nav-link" href="logout.php">Log Out</a></li>    
       </ul>
       </div>
     </nav>
